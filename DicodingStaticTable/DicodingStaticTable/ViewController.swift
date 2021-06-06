bf473188d1bd141ada3424d22782ac71faf2fc40
evanezcent <pratamayo16@gmail.com>
2021-06-06 21:19:58 +0700

    init table view

---
+++ DicodingStaticTable/DicodingStaticTable/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DicodingStaticTable
+//
+//  Created by Pratama Yoga on 06/06/21.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
